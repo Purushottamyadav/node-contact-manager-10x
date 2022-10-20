9b54bc49ab2d958d26688c73054b7a6286b10858
shitalkhante <shitalkhante786@gmail.com>
2022-10-20 21:24:00 +0530

    adding models

---
+++ Models/contacts.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const contactSchema = new mongoose.Schema({
+    email: {type:String, required:true},
+    name: {type:String, required:true},
+    designation: {type:String, required:true},
+    company: {type:String, required:true},
+    industry: {type:String, required:true},
+    phone: {type:Number, required:true},
+    country: {type:String, required:true},
+});
+
+module.exports = mongoose.model("user", userSchema);
