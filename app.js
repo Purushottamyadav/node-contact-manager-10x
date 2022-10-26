2eea7cb3fd519e8faca7f16eb0e5d6ada7d2c52a
pedagadi chandra sekhar <pedagadisekhar@gmail.com>
2022-10-26 20:19:56 +0530

    backend

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const bodyParser=require("body-parser")
+const cors = require("cors")
 
+app.use(cors())
 app.use(express.json())
 
 const contactroute = require("./routes/contact.js");
